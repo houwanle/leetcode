c301c3e1461fc95fc841b33da843932e0eb99ec8
wanle <wanle@qq.com>
2021-05-15 15:38:18 +0800

    添加 最长回文子串--暴力匹配

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ## leetcode 题解
 > java 语言实现
 
-- [两数之和](./src/com/lele/leetcode/algorithms/TwoSum.java)
-- [两数相加](./src/com/lele/leetcode/algorithms/AddTwoNumbers.java)
-- [无重复字符串最长子串](./src/com/lele/leetcode/algorithms/LongestSubString.java)
-- [寻找两个正序数组的中位数](./src/com/lele/leetcode/algorithms/FindMedianSortedArrays.java)
+- [leetcode_1：两数之和](./src/com/lele/leetcode/algorithms/TwoSum.java)
+- [leetcode_2：两数相加](./src/com/lele/leetcode/algorithms/AddTwoNumbers.java)
+- [leetcode_3：无重复字符串最长子串](./src/com/lele/leetcode/algorithms/LongestSubString.java)
+- [leetcode_4：寻找两个正序数组的中位数](./src/com/lele/leetcode/algorithms/FindMedianSortedArrays.java)
+- [leetcode_5：最长回文子串](./src/com/lele/leetcode/algorithms/LongestPalindrome.java)
+
+
---
+++ src/com/lele/leetcode/algorithms/LongestPalindrome.java
@@ -0,0 +1,52 @@
+package com.lele.leetcode.algorithms;
+
+/**
+ * @author: lele
+ * @date: 2021/5/11 19:22
+ * @description: 最长回文子串
+ */
+
+public class LongestPalindrome {
+    public static void main(String[] args) {
+        LongestPalindrome longestPalindrome = new LongestPalindrome();
+        String str = "bbbscsb";
+
+        System.out.println(longestPalindrome.longestPalindrome(str));
+    }
+
+    // 遍历所有长度大于2的子串
+    public String longestPalindrome(String str) {
+        int len = str.length();
+        if (len < 2) {
+            return str;
+        }
+
+        int maxLen = 1;
+        String result = str.substring(0, 1);
+
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (maxLen < j - i + 1 && valid(str, i, j)) {
+                    maxLen = j - i + 1;
+                    // 截取[i,j]
+                    result = str.substring(i, j + 1);
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean valid(String str, int left, int right) {
+        // 判断子串 [left,right]是否为回文字符串
+        while(left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
