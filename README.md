887f7f726eb281f800004ecf9f9f210226f7d1f9
wanle <wanle@qq.com>
2022-07-26 07:56:19 +0800

    添加 正整数1~N的阶乘之和

--- README.md
+++ README.md
@@ -21,6 +21,7 @@
 
 ## MSB
 - [打印十进制数的32位二进制信息](./src/com/lele/msb/PrintBinary.java)
+- [正整数1~N的阶乘之和](./src/com/lele/msb/SumOfFactorial.java)
 
 ## 其他
 - [二分查找](./src/com/lele/datastructure/search/BinarySearch.java)
---
+++ src/com/lele/msb/SumOfFactorial.java
@@ -0,0 +1,27 @@
+package com.lele.msb;
+
+/**
+ * @author: lele
+ * @date: 2022/7/26 上午7:49
+ * @description: 正整数1~N的阶乘之和
+ */
+
+public class SumOfFactorial {
+
+    public static void main(String[] args) {
+        int N = 5;
+        System.out.println(factorial(N));
+    }
+
+    private static long factorial(int N) {
+        long sum = 0;
+        long cur = 1;
+
+        for (int i = 1; i <= N; i++) {
+            cur = cur * i;
+            sum = sum + cur;
+        }
+
+        return sum;
+    }
+}
