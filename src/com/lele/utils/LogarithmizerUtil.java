cf86d0e91948553ef06a7e5fc1b56a31de3d2e82
wanle <wanle@qq.com>
2024-01-09 17:05:51 +0800

    更新 LogarithmizerUtil

--- src/com/lele/utils/LogarithmizerUtil.java
+++ src/com/lele/utils/LogarithmizerUtil.java
@@ -74,8 +74,9 @@ public class LogarithmizerUtil {
      * @return  生成的数组
      */
     public static int[] generateRandomArray(int maxSize, int maxValue) {
-//        Math.random()  [0, 1)
-//        Math.random() * N  [0, N)
+//        Math.random()  [0, 1) 所有的小数，等概率返回一个
+//        Math.random() * N  [0, N) 所有小数，等概率返回一个
+//        (int)(Math.random() * N)  [0, N-1] 所有的整数，等概率返回一个
         int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
         for (int i = 0; i < arr.length; i++){
 //            [-?, +?]
